504b25803b707d2080c86291a707ce4f7cca9631
heloisalourenc <157432733+heloisalourenc@users.noreply.github.com>
2024-12-05 09:35:01 -0300

    Aula 2 - Phyton Senai.
    Conhecimentos em string, boolean e foat.

---
+++ aula02.py
@@ -0,0 +1,15 @@
+print('Ola, mundo!')
+
+nome_usuario = 'Heloisa' # o que voces quiserem str = string
+idade_usuario = 23 # tipo int = integer 
+altura_usuario = 1.53 # tipo foat = ponto flutuante
+maior_idade =  False # tipo bool = boolean
+
+print('Seja bem vindo',nome_usuario)
+
+
+
+
+
+
+
